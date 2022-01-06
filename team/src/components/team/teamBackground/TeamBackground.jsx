4e54e99652fa7254309b73c3adbecfb9f4cddf58
HardikBhanot <hardikbhanot123@gmail.com>
2022-01-06 23:58:04 +0530

    first commit

---
+++ team/src/components/team/teamBackground/TeamBackground.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import  './TeamBackground.scss';
+function TeamBackground() {
+    return (
+      <div className='tBackground'>
+    <section className="team-background">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+  </section>
+  </div>  
+    )
+}
+
+export default TeamBackground;
